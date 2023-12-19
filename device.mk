85884b81386c620318d4fa0d98e6273021ecce32
bengris32 <bengris32@protonmail.ch>
2023-12-19 15:25:28 +0000

    ruby: Move to common sensors service
    Change-Id: Idebada879d5c64d59c2bc2ee06146f33b862538d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -352,7 +352,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.ruby-multihal
+    android.hardware.sensors@2.1-service.xiaomi-multihal
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
