d3b40fb28ef776f5db64d02e06c01da019be626f
bengris32 <bengris32@protonmail.ch>
2023-12-20 00:36:24 +0000

    ruby: Ship AOSP cgroups and task_profiles configs
    Change-Id: Ie74f85fa6328e6fa91d261c4aec257e5bb53ba0e
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -93,8 +93,8 @@ PRODUCT_PACKAGES += \
 
 # Cgroup
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Display
 PRODUCT_PACKAGES += \
