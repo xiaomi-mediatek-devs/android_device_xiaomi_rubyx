32a7496753b1fe4dd12c864af62a3a23a08d67d1
bengris32 <bengris32@protonmail.ch>
2024-02-28 22:38:02 +0000

    ruby: parts: Import from sm8250-common
    * From LineageOS/android_device_xiaomi_sm8250-common @ 62e8e0551849a1c2d4f1d35a328d5eb70330bf53
    * Drop all features apart from thermal.
    
    Change-Id: I39173f743d0d29dbcc3d88390830ed33570fed56
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -262,6 +262,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
