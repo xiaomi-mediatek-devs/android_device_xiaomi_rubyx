5d008f81453793eb11837fb43bc7d5f7e05d14bc
bengris32 <bengris32@protonmail.ch>
2024-07-08 16:44:44 +0100

    rubyx: Update fingerprint from HyperOS `V816.0.4.0.UMOMIXM`
    Change-Id: I82a8935ca138289733a0b1766b43faba98297115
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- lineage_rubyx.mk
+++ lineage_rubyx.mk
@@ -26,6 +26,6 @@ PRODUCT_SYSTEM_DEVICE := ruby
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ruby-user 14 UP1A.230620.001 V816.0.1.0.UMOMIXM release-keys"
+    PRIVATE_BUILD_DESC="ruby-user 14 UP1A.231005.007 V816.0.4.0.UMOMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/ruby_global/ruby:14/UP1A.230620.001/V816.0.1.0.UMOMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/ruby_global/ruby:14/UP1A.231005.007/V816.0.4.0.UMOMIXM:user/release-keys
