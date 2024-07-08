e5802886c6a3a8da65e5267205b014a2f1c77b63
Giovanni Ricca <giovanniricca@proton.me>
2024-07-08 16:48:29 +0100

    rubyx: Patch libsource to load the old graphicsbuffer symbol
    Change-Id: Ic8f1880379bf7adf07193842a710a104e434d470

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,9 @@ function blob_fixup {
         system_ext/lib64/libsink.so)
             grep -q "libshim_sink.so" "$2" || "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
             ;;
+        system_ext/lib64/libsource.so)
+            grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            ;;
     esac
 }
 
