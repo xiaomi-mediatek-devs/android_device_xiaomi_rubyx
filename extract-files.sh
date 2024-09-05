734a8318e6520d0caf161eb6dd06dc6b06a3ae54
Michael Bestas <mkbestas@lineageos.org>
2024-09-05 19:46:51 +0100

    rubyx: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -16,6 +15,10 @@ MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -33,22 +36,23 @@ SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-firmware )
-                ONLY_FIRMWARE=true
-                ;;
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
     esac
     shift
 done
@@ -58,42 +62,61 @@ if [ -z "${SRC}" ]; then
 fi
 
 function blob_fixup {
-    case "$1" in
+    case "${1}" in
         system_ext/priv-app/ImsService/ImsService.apk)
+            [ "$2" = "" ] && return 0
             apktool_patch "${2}" "${MY_DIR}/blob-patches/ImsService.patch" -r
             ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
-            grep -q "libstagefright_foundation-v33.so" "$2" || "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "$2"
+            [ "$2" = "" ] && return 0
+            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
-            sed -i 's/start/enable/' "$2"
+            [ "$2" = "" ] && return 0
+            sed -i 's/start/enable/' "${2}"
             ;;
         vendor/firmware/txpowerctrl_*)
-            sed -i 's/\t//g' "$2"
+            [ "$2" = "" ] && return 0
+            sed -i 's/\t//g' "${2}"
             ;;
         vendor/bin/mnld|\
         vendor/lib*/libaalservice.so)
-            grep -q "libshim_sensors.so" "$2" || "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
+            [ "$2" = "" ] && return 0
+            grep -q "libshim_sensors.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
             ;;
         vendor/lib*/hw/audio.primary.mt6877.so)
-            grep -q "libstagefright_foundation-v33.so" "$2" || "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "$2"
-            "$PATCHELF" --replace-needed "libalsautils.so" "libalsautils-v31.so" "$2"
+            [ "$2" = "" ] && return 0
+            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
             ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.14-impl.so)
-            "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
-            grep -q "libshim_sensors.so" "$2" || "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            grep -q "libshim_sensors.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sensors.so" "${2}"
             ;;
         vendor/lib*/libmtkcam_stdutils.so|\
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         system_ext/lib64/libsink.so)
-            grep -q "libshim_sink.so" "$2" || "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
+            [ "$2" = "" ] && return 0
+            grep -q "libshim_sink.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sink.so" "${2}"
             ;;
         system_ext/lib64/libsource.so)
+            [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "${1}" ""
 }
 
 # Initialize the helper
