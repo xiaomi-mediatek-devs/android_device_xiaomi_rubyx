b6e2c30bdb06adf3f47f78d7f651a53e92292e46
Aaron Kling <webgeek1234@gmail.com>
2024-09-05 19:58:33 +0100

    rubyx: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -23,6 +23,15 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+	cat <<EOF >>"$1"
+		"device/xiaomi/rubyx",
+		"hardware/mediatek",
+		"hardware/mediatek/libmtkperf_client",
+		"hardware/xiaomi"
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
