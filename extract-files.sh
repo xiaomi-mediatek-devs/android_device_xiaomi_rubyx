03694c399eec25cfa7ac3299a63b9cd8e6861237
Jiyong Park <jiyong@google.com>
2024-09-06 19:24:15 +0100

    rubyx: gnss: Remove ndk_platform backend. Use the ndk backend.
    The ndk_platform backend will soon be deprecated because the ndk backend
    can serve the same purpose. This is to eliminate the confusion about
    having two variants (ndk and ndk_platform) for the same ndk backend.
    
    Bug: 161456198
    Test: m
    Merged-In: Id16039722fd70d4e2e934a0988f13a768ce833ec
    Change-Id: Id16039722fd70d4e2e934a0988f13a768ce833ec

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,10 @@ function blob_fixup {
             [ "$2" = "" ] && return 0
             apktool_patch "${2}" "${MY_DIR}/blob-patches/ImsService.patch" -r
             ;;
+        vendor/bin/hw/android.hardware.gnss-service.mediatek|\
+        vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
+            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
+            ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
             [ "$2" = "" ] && return 0
             grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
